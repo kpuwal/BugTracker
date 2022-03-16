bf79928f77f5f3cdd0e04a980d415e0d709e7dc2
kpuwal <kpuwal@users.noreply.github.com>
2022-03-16 20:27:23 +0100

    adds dotenv to client

--- backend/db/models/bug.ts
+++ backend/db/models/bug.ts
@@ -1,5 +1,9 @@
 import { ObjectId } from "mongodb";
 
 export default class Bug {
-  constructor(public name: string, public description: string, public category: string, public id?: ObjectId) {}
+  constructor(
+    public name: string, 
+    public description: string, 
+    public category: string, 
+    public id?: ObjectId) {}
 }
